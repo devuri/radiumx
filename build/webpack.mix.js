7c9a79b5876dd5295cc7ae4de100828781ebb32d
devuri <devuri@users.noreply.github.com>
2024-07-17 08:40:19 -0500

    init

---
+++ build/webpack.mix.js
@@ -0,0 +1,21 @@
+let mix = require('laravel-mix')
+
+mix.setPublicPath('build').version()
+
+mix.options({ manifest: false })
+
+// build web application in trunk dir.
+mix.copy('_htpass', 'build/trunk/_htpass')
+mix.copy('vendor', 'build/trunk/vendor')
+mix.copy('public', 'build/trunk/public')
+mix.copy('asset', 'build/trunk/public/asset/dist')
+
+mix.copy([
+    'LICENSE',
+    'bootstrap.php',
+    'composer.json',
+    'composer.lock',
+    'composer.lock',
+    'config.php',
+    'wp-cli.yml'
+], 'build/trunk/')
